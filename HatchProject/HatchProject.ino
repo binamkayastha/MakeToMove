2d5979049f7ffd8249fa9c0373bf0cf9944ab355
Binam K <bkayastha@wpi.edu>
2015-08-02 11:41:05 -0400

    Changed file

--- HatchProject/HatchProject.ino
+++ HatchProject/HatchProject.ino
@@ -1,150 +1,112 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
 unsigned long startTime;
 int state = 0;
 int count = 0;
-// the setup function runs once when you press reset or power the board
+int LEDsOn[] = {0,1,2,3,4,5};
+int currentState;
+
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(9, INPUT);
-  
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  //3 Inputs into the decoder to produce 8 Inputs
+  pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(10, OUTPUT);
-  startTime = millis();
-  Serial.begin(9600);
+
+  //The input of wheel excersize machine
+  pinMode(10, INPUT);
+
+  //The input of other excersize macine
+  pinMode(11, INPUT);
+
+  //G2A and G2B Low (Connected in circuitry)
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  int currentState = digitalRead(9);
-  Serial.println(currentState);
-  
-  if(state == currentState) {
-    count++;
-  } else if (state != currentState) {
-    count = 0;
-    if(millis()-startTime > 50000) {
-      Serial.print("This is current time: ");
-      Serial.println(millis());
-      Serial.print("This is the start time: ");
-      Serial.println(startTime);
-      Serial.print("You've been running for 1 while");
-      /*
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      */     
-    
-    } else if (millis()-startTime > 40000) {
-      Serial.print("You've been running for 2 while");
-      /*
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      */     
-    } else if (millis()-startTime > 30000) {
-      digitalWrite(3, HIGH);
-      /*
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      */
-    } else if (millis()-startTime > 20000) {
-      digitalWrite(4, HIGH);
-      /*
-      digitalWrite(10, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      */
-    } else if (millis()-startTime > 10000) {
-      digitalWrite(5, HIGH);
-      /*
-      digitalWrite(10, HIGH);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(10, LOW);
-      */
-    }
-    
-    state = currentState;
+  // put your main code here, to run repeatedly:
+
+//  Serial.println("Starting");
+
+  // UPDATE LEDsOn
+//  Serial.println(sizeof(LEDsOn));
+  //turnOnLEDsAt(LEDsOn);
+//  delay(1000);
+
+  currentState = digitalRead(10);
+  if(millis()-startTime > 60000) {
+    turnOnLEDsAt(LEDsOn, 5);
+  } else if(millis()-startTime > 50000) {
+    turnOnLEDsAt(LEDsOn, 4);
+  } else if (millis()-startTime > 40000) {
+    turnOnLEDsAt(LEDsOn, 3);
+  } else if (millis()-startTime > 30000) {
+    turnOnLEDsAt(LEDsOn, 2);
+  } else if (millis()-startTime > 7000) {
+    turnOnLEDsAt(LEDsOn, 1);
+  } else if (millis()-startTime > 5000) {
+    turnOnLEDsAt(LEDsOn, 0);
   }
-  
-  if (count >50) {
+  if (state != currentState) {
+    state = currentState;
+    count = 0; 
+  } else {
+    count++;
+  } 
+
+  if (count > 50) {
     Serial.println("Reset");
-    digitalWrite(5, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(3, LOW);
-    Serial.print("This is current time: ");
-    Serial.println(millis());
-    Serial.print("This is the start time: ");
-    Serial.println(startTime);
+    turnOnLEDsAt(LEDsOn, 0);
       
     startTime = millis();
-    Serial.print("bleh");
-    Serial.print("This is current time: ");
-    Serial.println(millis());
-    Serial.print("This is the start time: ");
-    Serial.println(startTime);
     count = 0;
-  }
-  
-//  if((state == currentState) && ((millis()-startTime)>1000) && !wasChangedRecently) {
-//    startTime = millis();
-//    wasChangedRecently = false;
-//    Serial.println("RESTARTED");
-//  }
-//  if((state!=currentState) && (millis()-startTime>1000)) {
-//    Serial.println("You've been running for a while");
-//    state=currentState;
-//    wasChangedRecently = true;
-//  }
-  
-//  if(state!=currentState) {
-//    startTime = millis();
-//    state = currentState;
-//    Serial.println("RESTARTED");
-//  }
-//  if (millis() > (startTime+10000)) {
-//    Serial.println("You've been pedeling for some time");
-//  }
-  delay(100);
+  } 
+  delay(25);
 }
+
+void turnOnLEDsAt(int LEDs[], int level){
+  Serial.print("At LEVEL: ");
+  Serial.println(level);
+  for(int i=0; i < level+1; i++){
+//    Serial.println(sizeof(LEDs));
+//    Serial.print("LED ON:: ");
+//    Serial.println(LEDs[i]);
+    turnOnLEDAt(LEDs[i]);
+  }
+}
+
+void turnOnLEDAt(int pos, int machine){
+  int a, b, c;
+  a = b = c = 0;
+  
+  // assume pos max is 8
+  a = pos %2;
+  b = ((int) (pos / 2)) % 2;
+  c = pos / 4;
+  
+////  Serial.print("a: ");
+//  Serial.println(a);
+////  Serial.print("b: ");
+// Serial.println(b);
+////  Serial.print("c: ");
+//  Serial.println(c);
+////  Serial.println("---------");
+  if(machine == 1) {
+    digitalWrite(2, c);
+    digitalWrite(3, b);
+    digitalWrite(4, a);
+  } else if (machine == 2) {
+    digitalWrite(5, c);
+    digitalWrite(6, b);
+    digitalWrite(7, a);
+  }
+//  digitalWrite(2, LOW);
+//  digitalWrite(3, LOW);
+//  digitalWrite(4, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(50);
+}
+
+
